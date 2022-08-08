350086b8e882f21837c963058ccc4c60a4cb712c
serduero <serduero@gmail.com>
2022-08-08 21:04:28 +0200

    sin acceso bbdd

--- src/controllers/database.js
+++ src/controllers/database.js
@@ -1,11 +1,3 @@
-/* const database =
-  {
-    host: 'b5s1p7ubh0ujcnb6jdxc-mysql.services.clever-cloud.com',
-    user: 'u2svqk5ihhqfkfab',
-    password: 'QmEr4Kh7yrgGcH0nKFcw',
-    database: 'b5s1p7ubh0ujcnb6jdxc',
-};*/
-
 const database =
   {
     host: process.env.DATABASE_HOST,user: process.env.DATABASE_USER,
