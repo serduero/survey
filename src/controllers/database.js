1106940d517fad232e8a2c2cc21494d9542e5c01
serduero <72622989+serduero@users.noreply.github.com>
2022-08-08 20:37:35 +0200

    Update database.js

--- src/controllers/database.js
+++ src/controllers/database.js
@@ -1,15 +1,7 @@
-const database = {
-    host: "b5s1p7ubh0ujcnb6jdxc-mysql.services.clever-cloud.com",
-    user:  "u2svqk5ihhqfkfab",
-    password: "QmEr4Kh7yrgGcH0nKFcw",
-    database: "b5s1p7ubh0ujcnb6jdxc",
-  };
-
-/*
 const database =
   {
     host: process.env.DATABASE_HOST,user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,database: process.env.DATABASE_NAME,
-};*/
+};
 
 export { database };
