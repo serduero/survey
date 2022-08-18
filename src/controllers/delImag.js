544759c3c2d6eb8c88639e680c1a0579bcada603
serduero <serduero@gmail.com>
2022-08-18 11:10:09 +0200

    borrado masivo

--- src/controllers/delImag.js
+++ src/controllers/delImag.js
@@ -1,31 +1,59 @@
-import { unlink, existsSync } from 'fs';
+import { unlink, existsSync, readdir, unlinkSync } from 'fs';
 
 const delImag = (req, res) => {
 
     // console.log('en delete');
     // console.log(req.headers['file']);
 
-    // if (req.headers['file'][0] === '*') {
-    //     // borrar todos los ficheros de un usuario
-    //     res.send('KO');
-    // }
-    // else {
+    if (req.headers['file'][0] === '*') {
+        
+        let idUsuario = req.headers['file'].slice(1);
+        var numero = 0;
+         
+        // console.log(idUsuario);
+
+        readdir('./src/public/images/', async (err, files) => {
+            if (err) throw err;
+
+            // Sólo entra aquí una vez, y deja en files el vector de
+            // ficheros encontrado
+            var fichero = '';
+
+            for await (const file of files) {
+                
+              if (file.split('-')[1] == idUsuario) {
+                fichero = './src/public/images/' + file;
+                // console.log('--> ', fichero);
+
+                unlinkSync(fichero, (err) => {
+                    if (err) {
+                        res.send({resul: 'KO', adicional:err.toString()});
+                        return;
+                    }
+                });
+                numero ++;
+              }
+            }
+            res.send({resul: 'OK', adicional: numero.toString()});
+        });
+    }
+    else {
         // borrar sólo el demandado
         let fichero = './src/public/images/' + req.headers['file'];
         
         if (existsSync(fichero)) {
             unlink(fichero, (err) => {
                 if (err) {
-                    res.send(err.tostring());
+                    res.send({resul: 'KO', adicional:err.toString()});
                 }
                 else {
-                    res.send('');
+                    res.send({resul:'OK', adicional: '1'});
                 }
             });
         } else {
-            res.send('');
+            res.send({resul: 'OK', adicional:'0'});
         }
-    // }
+    }
 }
   
 export default delImag;
